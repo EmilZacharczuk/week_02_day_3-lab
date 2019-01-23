38d1a340452ee2bc344d1761b2ba04b26d618c0e
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-01-23 15:19:38 +0000

    pass include guess test

--- hiddenword.rb
+++ hiddenword.rb
@@ -15,38 +15,20 @@ attr_reader :word, :word_array
     @word_array.join("")
   end
 
-  def compare_guesses_to_word(game)
-    display_array = []
-    convert_word_into_array
-
-    # count = 0
-    for letter in @word_array
-      # count += 1
-      # return if count == @word_array.count
-
-      for guessed_letter in game.guessed_letters
-
-        if letter == guessed_letter
-          display_array << letter
-
-        # else
-        #   display_array << "*"
-        end
-         # display_array << "*"
-      end
-
-      if display_array.last != "*"
-        display_array = display_array
-      else
-        display_array << "*" #unless theres no more letters in the array
-      end # when letter's index in wordarray == wordarray[-1]
-
-    end
-    # display_array << "*"
-
-    return display_array.join("")
-
+  def hidden_word_include_guess(guess)
+    @word_array.include?(guess)
   end
+  # def compare_guesses_to_word(game, guess)
+  #   display_array =
+  #   convert_word_into_array
+  #
+  #   if @word_array.include?(guess) == true
+  #
+  #
+  #
+  #   return display_array.join("")
+  #
+  # end
 
   end
 
--- specs/hiddenword_spec.rb
+++ specs/hiddenword_spec.rb
@@ -9,6 +9,7 @@ class HiddenwordTest < MiniTest::Test
 
   def setup
     @hiddenword = Hiddenword.new("codeclan")
+    @guess_1 = "a"
     #create a game instance
     @player_1 = Player.new("John")
     @game = Game.new(@player_1,@hiddenword)
@@ -27,14 +28,20 @@ class HiddenwordTest < MiniTest::Test
     assert_equal('codeclan', @hiddenword.convert_array_into_word)
   end
 
-  def test_if_guessed_letters_matches_hiddenword_array
-    @game.add_letter_to_guessed_letters("a")
-    assert_equal("******a*", @hiddenword.compare_guesses_to_word(@game))
-  end
+  # def test_if_guessed_letters_matches_hiddenword_array
+  #   @game.add_letter_to_guessed_letters("a")
+  #   assert_equal("******a*", @hiddenword.compare_guesses_to_word(@game))
+  # end
+  #
+  # def test_if_guessed_letters_matches_hiddenword_array__2_letters
+  #   @game.add_letter_to_guessed_letters(@guess_1)
+  #   @game.add_letter_to_guessed_letters("c")
+  #   assert_equal("******a*", @hiddenword.compare_guesses_to_word(@game, @guess_1))
+  # end
 
-  def test_if_guessed_letters_matches_hiddenword_array__2_letters
-    @game.add_letter_to_guessed_letters("a")
-    @game.add_letter_to_guessed_letters("c")
-    assert_equal("******a*", @hiddenword.compare_guesses_to_word(@game))
+  def test_if_hidden_word_include_guess
+    @hiddenword.convert_word_into_array
+
+    assert_equal(true, @hiddenword.hidden_word_include_guess(@guess_1))
   end
 end
